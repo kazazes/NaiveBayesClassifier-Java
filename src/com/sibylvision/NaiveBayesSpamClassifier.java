104f8058c4dcb7ec139deeac2a90bc7bf1baf80e
Peter Kazazes <peter@sibylvision.com>
2014-09-17 13:34:56 -0400

    Make corpus location user configurable

--- src/com/sibylvision/NaiveBayesSpamClassifier.java
+++ src/com/sibylvision/NaiveBayesSpamClassifier.java
@@ -1,39 +1,66 @@
 package com.sibylvision;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.Set;
 
 public class NaiveBayesSpamClassifier {
-	
+
 	private HashMap<String, Integer[]> featureCategory;
 	private HashMap<NaiveBayesSpamClassification, Integer> categoryUsage;
 	private static final double ASSUMED_PROBABILITY_WEIGHT = 1.0;
 	private static final double ASSUMED_PROBABILITY = 0.5;
-	
-	private static final String GOOD_CORPUS_LOCATION = "/Users/pk/Development/eclipse/NaiveBayesClassifier/src/com/sibylvision/good.corpus";
-	private static final String SPAM_CORPUS_LOCATION = "/Users/pk/Development/eclipse/NaiveBayesClassifier/src/com/sibylvision/spam.corpus";
-	
-	public NaiveBayesSpamClassifier(String goodCorpusLocation, String spamCorpusLocation) {
+
+	public NaiveBayesSpamClassifier() {
 		featureCategory = new HashMap<String, Integer[]>();
 		categoryUsage = new HashMap<NaiveBayesSpamClassification, Integer>();
-		
+
+		Properties prop = new Properties();
+		InputStream input = null;
+		String goodLocation = "";
+		String badLocation = "";
+		try {
+
+			input = new FileInputStream("classifier.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			goodLocation = prop.getProperty("GOOD_LOCATION");
+			badLocation = prop.getProperty("SPAM_LOCATION");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+
 		List<String> goodCorpus;
 		List<String> spamCorpus;
-		
+
 		try {
-			goodCorpus = Files.readAllLines(Paths.get(goodCorpusLocation), StandardCharsets.UTF_8);
-			spamCorpus = Files.readAllLines(Paths.get(spamCorpusLocation), StandardCharsets.UTF_8);
-			
+			goodCorpus = Files.readAllLines(Paths.get(goodLocation), StandardCharsets.UTF_8);
+			spamCorpus = Files.readAllLines(Paths.get(badLocation), StandardCharsets.UTF_8);
+
 			for (String a : goodCorpus) {
 				train(a, NaiveBayesSpamClassification.GOOD_CLASSIFICATION);
 			}
-			
+
 			for (String a : spamCorpus) {
 				train(a, NaiveBayesSpamClassification.BAD_CLASSIFICATION);
 			}
@@ -42,18 +69,13 @@ public class NaiveBayesSpamClassifier {
 			e.printStackTrace();
 		}
 	}
-	
-	public NaiveBayesSpamClassifier() {
-		this(GOOD_CORPUS_LOCATION, SPAM_CORPUS_LOCATION);
-	}
-
 
 	public NaiveBayesSpamClassification classify(String document) {
 		Double[] values = new Double[2];
 		for (NaiveBayesSpamClassification c : categories()) {
 			values[c.arrayLocation()] = probabilityOfCategory(document, c);
 		}
-		
+
 		if (values[0] / NaiveBayesSpamClassification.GOOD_CLASSIFICATION.classificationThreshold() > values[1]) {
 			return NaiveBayesSpamClassification.GOOD_CLASSIFICATION;
 		} else if (values[1] / NaiveBayesSpamClassification.BAD_CLASSIFICATION.classificationThreshold() > values[0]) {
@@ -62,7 +84,7 @@ public class NaiveBayesSpamClassifier {
 			return NaiveBayesSpamClassification.NO_CLASSIFICATION; 
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		NaiveBayesSpamClassifier self = new NaiveBayesSpamClassifier();
 		Scanner scan = null;
@@ -82,11 +104,11 @@ public class NaiveBayesSpamClassifier {
 			}
 		}
 	}
-	
+
 	String[] getFeatures(String text) {
 		return text.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
 	}
-	
+
 	private int increaseFeatureFrequencyClassificationCount(String word, NaiveBayesSpamClassification classification) {
 		if (featureCategory.containsKey(word)) {
 			Integer[] array = featureCategory.get(word);
@@ -112,7 +134,7 @@ public class NaiveBayesSpamClassifier {
 		}
 		return 0;
 	}
-	
+
 	private int increaseCategoryCount(NaiveBayesSpamClassification classification) {
 		if (categoryUsage.containsKey(classification)) {
 			categoryUsage.put(classification, categoryUsage.get(classification) + 1);
@@ -122,7 +144,7 @@ public class NaiveBayesSpamClassifier {
 			return 1;
 		}
 	}
-	
+
 	private int featureClassificationCount(String word, NaiveBayesSpamClassification classification) {
 		if (featureCategory.containsKey(word)) {
 			return featureCategory.get(word)[classification.arrayLocation()];
@@ -130,7 +152,7 @@ public class NaiveBayesSpamClassifier {
 			return 0;
 		}
 	}
-	
+
 	private double classificationCount(NaiveBayesSpamClassification classification) {
 		if (categoryUsage.containsKey(classification)) {
 			return categoryUsage.get(classification);
@@ -138,7 +160,7 @@ public class NaiveBayesSpamClassifier {
 			return 0;
 		}
 	}
-	
+
 	private int totalClassifications() {
 		int sum = 0;
 		for (Integer a : categoryUsage.values()) {
@@ -146,11 +168,11 @@ public class NaiveBayesSpamClassifier {
 		}
 		return sum;
 	}
-	
+
 	private Set<NaiveBayesSpamClassification> categories() {
 		return categoryUsage.keySet();
 	}
-	
+
 	private double featureClassificationProbability(String feature, NaiveBayesSpamClassification classification) {
 		if (featureCategory.containsKey(feature)) {
 			return (double) featureClassificationCount(feature, classification) / classificationCount(classification);
@@ -158,43 +180,43 @@ public class NaiveBayesSpamClassifier {
 			return 0;
 		}
 	}
-	
+
 	private double weightedFeatureClassificationProbability(String feature, NaiveBayesSpamClassification classification) {
 		double basicProbability = featureClassificationProbability(feature, classification);
-		
+
 		int totalClassifications = 0;
 		for (NaiveBayesSpamClassification c : categories()) {
 			totalClassifications += featureClassificationCount(feature, c);
 		}
-		
+
 		double weightedProbability = ((ASSUMED_PROBABILITY_WEIGHT * ASSUMED_PROBABILITY) + (totalClassifications * basicProbability)) / (ASSUMED_PROBABILITY_WEIGHT + totalClassifications);
 		return weightedProbability;
 	}
-	
+
 	private double documentProbability(String document, NaiveBayesSpamClassification classification) {
 		String[] features = getFeatures(document);
 		double propability = 1;
-		
+
 		for (String f : features) {
 			propability *= weightedFeatureClassificationProbability(f, classification);
 		}
-		
+
 		return propability;
 	}
-	
+
 	private double probabilityOfCategory(String document, NaiveBayesSpamClassification classification) {
 		double categoryProbability = classificationCount(classification) / totalClassifications();
 		double documentProbability = documentProbability(document, classification);
-		
+
 		return categoryProbability * documentProbability;
 	}
-	
+
 	private void train(String textBody, NaiveBayesSpamClassification classification) {
 		String[] features = getFeatures(textBody);
 		for (String word : features) {
 			increaseFeatureFrequencyClassificationCount(word, classification);
 		}
-		
+
 		increaseCategoryCount(classification);
 	}
 }
